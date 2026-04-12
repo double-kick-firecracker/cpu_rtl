ba9d48ba6481f34cc488f021bea802040b46d2c8
double-kick-firecracker <861129353@qq.com>
2026-04-12 10:37:45 +0800

    Update riscv.v

--- rtl/riscv.v
+++ rtl/riscv.v
@@ -11,26 +11,28 @@ module riscv(clk, rst);
     wire [31:0] in_ins;
     wire [31:0] NPC;
     wire IRWrite, PCWrite, InsMemRW;
+    wire StallF,StallD;
     
     assign IF_PC_plus_4 = IF_PC + 32'd4; 
     assign NPC = EX_Control_Taken ? EX_NPC_Target : IF_PC_plus_4;
     PC U_PC (
-        .clk(clk), .rst(rst), .PCWrite(PCWrite), .NPC(NPC), .PC(IF_PC)
+        .clk(clk), .rst(rst), .PCWrite(PCWrite), .NPC(NPC), .PC(IF_PC), .stall(StallF)
     );
     
     IM U_IM (
-        .addr(IF_PC[11:2]), .Ins(in_ins), .InsMemRW(InsMemRW)
+        .addr(IF_PC[11:2]), .Ins(in_ins), .InsMemRW(InsMemRW), .clk(clk), .stall(StallD)
     );
     
 //——————IF到ID的寄存器——————//
     wire [31:0] out_ins; 
     wire [31:0] ID_PC;
+    wire FlushD;
     Flopr U_IF_ID_PC (
-        .clk(clk), .rst(rst), .in_data(IF_PC), .out_data(ID_PC)
+        .clk(clk), .rst(rst), .in_data(IF_PC), .out_data(ID_PC), .CLR(FlushD), .Stall(StallD)
     );      
                 
     IR U_IR (
-        .clk(clk), .IRWrite(IRWrite), .in_ins(in_ins), .out_ins(out_ins)
+        .clk(clk), .IRWrite(IRWrite), .in_ins(in_ins), .out_ins(out_ins), .flush(FlushD)
     );   
     
 //————————ID——————// 
@@ -43,7 +45,12 @@ module riscv(clk, rst);
     wire RFWrite, DMCtrl, ExtSel,ALUSrcA;
     wire [11:0] Imm12;
     wire [31:0] Imm32;
-    wire [4:0] rs1, rs2;
+    wire [4:0] rs1, rs2,rd;
+    wire ID_Jump, ID_Branch;
+    wire [20:1] Offset20;
+    wire [11:0] Offset;
+    wire [4:0] ID_rs1;
+    wire [4:0] ID_rs2;
     
     assign opcode   = out_ins[6:0];
     assign Funct3   = out_ins[14:12];
@@ -51,12 +58,22 @@ module riscv(clk, rst);
     assign rs1      = out_ins[19:15];
     assign rs2      = out_ins[24:20];
     assign Imm12    = out_ins[31:20];
+    assign rd       = out_ins[11:7];
+    assign ID_rs1   = out_ins[19:15];
+    assign ID_rs2   = out_ins[24:20];
+    
+    assign Offset20 = {out_ins[31], out_ins[19:12], out_ins[20], out_ins[30:21]};//jal--需要传入到NPC中，
+    assign Offset   = (opcode == `INSTR_BTYPE_OP) ? {out_ins[31], out_ins[7], out_ins[30:25], out_ins[11:8]} :
+                      (opcode == `INSTR_SW_OP)   ? {out_ins[31:25], out_ins[11:7]} : Imm12;  //会在MUX3-1内部符号拓展
+                      
+    wire [31:0] ID_Offset_Packed;
+    assign ID_Offset_Packed = {Offset20, Offset};
 
     ControlUnit U_ControlUnit(
         .clk(clk), .rst(rst), .zero(1'b0), .opcode(opcode), .Funct7(Funct7), .Funct3(Funct3),//zero貌似悬空了，不知道影不影响，先给一个const
         .RFWrite(RFWrite), .DMCtrl(DMCtrl), .PCWrite(PCWrite), .IRWrite(IRWrite), .InsMemRW(InsMemRW), 
-        .ExtSel(ExtSel), .ALUOp(ALUOp), .NPCOp(NPCOp), .ALUSrcA(ALUSrcA),          
-        .WDSel(WDSel), .ALUSrcB(ALUSrcB), .RegSel(RegSel)
+        .ExtSel(ExtSel), .ALUOp(ALUOp), .NPCOp(NPCOp), .ALUSrcA(ALUSrcA), .WDSel(WDSel),
+        .ALUSrcB(ALUSrcB), .RegSel(RegSel), .Jump(ID_Jump), .Branch(ID_Branch)
     );
     
     wire [4:0] WB_WR;
@@ -74,52 +91,67 @@ module riscv(clk, rst);
     );
     
 //————————ID/EXE————————//
-    wire [31:0] EX_PC, EX_Imm32, EX_Inst, EX_RD1, EX_RD2;
+    wire [31:0] EX_PC, EX_Imm32, EX_Inst, EX_RD1, EX_RD2, EX_Offset_Packed;
+    wire FlushE;
     
-    Flopr U_A         ( .clk(clk), .rst(rst), .in_data(RD1),     .out_data(EX_RD1) );
-    Flopr U_B         ( .clk(clk), .rst(rst), .in_data(RD2),     .out_data(EX_RD2) );
-    Flopr U_IDEX_PC   ( .clk(clk), .rst(rst), .in_data(ID_PC),   .out_data(EX_PC) );
-    Flopr U_IDEX_IMM  ( .clk(clk), .rst(rst), .in_data(Imm32),   .out_data(EX_Imm32) );
-    Flopr U_IDEX_INST ( .clk(clk), .rst(rst), .in_data(out_ins), .out_data(EX_Inst) );
-
-    // 打包控制信号 (ID -> EX)
-    wire [31:0] id_ctrl_pack = {17'b0, ALUOp, NPCOp, ALUSrcB, ALUSrcA, DMCtrl, RFWrite, WDSel, RegSel};
-    // 位域分布: [14:11]ALUOp, [10:9]NPCOp, [8:7]ALUSrcB, [6]ALUSrcA, [5]DMCtrl, [4]RFWrite, [3:2]WDSel, [1:0]RegSel
-    wire [31:0] ex_ctrl;
-    Flopr U_IDEX_CTRL ( .clk(clk), .rst(rst), .in_data(id_ctrl_pack), .out_data(ex_ctrl) );
+    Flopr U_A         ( .clk(clk), .rst(rst), .in_data(RD1),     .out_data(EX_RD1),.CLR(FlushE), .Stall(1'b0) );
+    Flopr U_B         ( .clk(clk), .rst(rst), .in_data(RD2),     .out_data(EX_RD2),.CLR(FlushE), .Stall(1'b0) );
+    Flopr U_IDEX_PC   ( .clk(clk), .rst(rst), .in_data(ID_PC),   .out_data(EX_PC),.CLR(FlushE), .Stall(1'b0) );
+    Flopr U_IDEX_IMM  ( .clk(clk), .rst(rst), .in_data(Imm32),   .out_data(EX_Imm32) ,.CLR(FlushE), .Stall(1'b0));
+    Flopr U_IDEX_OFFSET ( .clk(clk), .rst(rst), .in_data(ID_Offset_Packed), .out_data(EX_Offset_Packed),.CLR(FlushE), .Stall(1'b0) );
+    
+    wire       ID_funct3_0 = out_ins[12]; // 用来区别btype
+    
+    reg [32:0] id_ex_ctrl_reg;
+    always @(posedge clk or posedge rst) begin
+        if (rst) 
+            id_ex_ctrl_reg <= 33'b0;
+        else 
+            id_ex_ctrl_reg <= {
+                ID_rs1,ID_rs2,
+                ID_Branch, ID_Jump, ID_funct3_0, rd,          
+                ALUOp, NPCOp, ALUSrcB, ALUSrcA,               
+                DMCtrl, RFWrite, WDSel, RegSel                
+            };
+    end
     
 //——————EXE——————//
-    wire [20:1] Offset20;
-    wire [11:0] Offset;
-    wire [6:0]  EX_opcode;   
-    
-    assign EX_opcode = EX_Inst[6:0];
-    assign Offset20 = {EX_Inst[31], EX_Inst[19:12], EX_Inst[20], EX_Inst[30:21]};//jal--需要传入到NPC中，
-    assign Offset   = (EX_opcode == `INSTR_BTYPE_OP) ? {EX_Inst[31], EX_Inst[7], EX_Inst[30:25], EX_Inst[11:8]} :
-                      (EX_opcode == `INSTR_SW_OP)   ? {EX_Inst[31:25], EX_Inst[11:7]} : EX_Inst[31:20];  //会在MUX3-1内部符号拓展
-                      
-    // 提取 EX 阶段需要的控制信号
-    wire [3:0] EX_ALUOp   = ex_ctrl[14:11];
-    wire [1:0] EX_NPCOp   = ex_ctrl[10:9];
-    wire [1:0] EX_ALUSrcB = ex_ctrl[8:7];
-    wire       EX_ALUSrcA = ex_ctrl[6];
+    wire [4:0] EX_rs1 = id_ex_ctrl_reg[32:28];
+    wire [4:0] EX_rs2 = id_ex_ctrl_reg[27:23];
+    wire EX_Branch      = id_ex_ctrl_reg[22];
+    wire EX_Jump        = id_ex_ctrl_reg[21];
+    wire EX_funct3_0    = id_ex_ctrl_reg[20];
+    wire [4:0] EX_rd    = id_ex_ctrl_reg[19:15];
+    wire [3:0] EX_ALUOp = id_ex_ctrl_reg[14:11];
+    wire [1:0] EX_NPCOp = id_ex_ctrl_reg[10:9];
+    wire EX_ALUSrcB     = id_ex_ctrl_reg[8:7];
+    wire EX_ALUSrcA     = id_ex_ctrl_reg[6];
+    wire EX_DMCtrl      = id_ex_ctrl_reg[5];
+    wire EX_RFWrite     = id_ex_ctrl_reg[4];
+    wire [1:0]EX_WDSel  = id_ex_ctrl_reg[3:2]; 
+    wire [1:0]EX_RegSel = id_ex_ctrl_reg[1:0];
+    wire [19:0] EX_Offset20 = EX_Offset_Packed[31:12];
+    wire [11:0] EX_Offset12 = EX_Offset_Packed[11:0];
 
     wire [31:0] A, B, ALU_result;
     wire zero;
     wire [31:0] PCA4;                  
     NPC U_NPC (
-        .PC(EX_PC), .NPCOp(EX_NPCOp), .Offset12(Offset), .Offset20(Offset20), .rs({EX_RD1[31:2],2'b00}),
+        .PC(EX_PC), .NPCOp(EX_NPCOp), .Offset12(Offset), .Offset20(Offset20), .rs({Forwarded_A[31:2],2'b00}),
         .imm(EX_Imm32), .PCA4(PCA4),.NPC(EX_NPC_Target)//修改了rs接入的数
     );
     
+    wire [31:0] Forwarded_A;
+    wire [31:0] Forwarded_B;//包含了前递情况的预先声明
+    
     // 实例化 MUX_2to1_A——决定ALUA的操作数来源
     MUX_2to1_A U_MUX_2to1_A (
-        .X(EX_RD1), .Y(32'h0), .control(EX_ALUSrcA), .out(A)
+        .X(Forwarded_A), .Y(32'h0), .control(EX_ALUSrcA), .out(A)
     );
 
     // 实例化 MUX_3to1_B——ALUB的操作数来源
     MUX_3to1_B U_MUX_3to1_B (
-        .X(EX_RD2), .Y(EX_Imm32), .Z(Offset), .control(EX_ALUSrcB), .out(B)
+        .X(Forwarded_B), .Y(EX_Imm32), .Z(Offset), .control(EX_ALUSrcB), .out(B)
     );
 
     // 实例化 ALU
@@ -128,48 +160,62 @@ module riscv(clk, rst);
     );
     
     // 分支判定 (反馈给 IF 阶段)
-    wire EX_is_Branch = (EX_opcode == `INSTR_BTYPE_OP);//其实可以在CU里面加接口JUMP和BRANCH传过来，不知道哪个更方便更省面积
-    wire EX_is_Jump   = (EX_opcode == `INSTR_JAL_OP) || (EX_opcode == `INSTR_JALR_OP);
-    wire EX_Branch_Cond_Met = (EX_Inst[12] == 1'b1) ? ~zero : zero;
-    assign EX_Control_Taken = EX_is_Jump || (EX_is_Branch && EX_Branch_Cond_Met);
+    wire EX_Branch_Cond_Met = (EX_funct3_0 == 1'b1) ? ~zero : zero;
+    assign EX_Control_Taken = EX_Jump || (EX_Branch && EX_Branch_Cond_Met);
 
 //————————EX/MEM——————//
     wire [31:0] MEM_ALU_result, MEM_RD2, MEM_PCA4, MEM_Inst, mem_ctrl;
     
-    Flopr U_ALUOut (
-        .clk(clk), .rst(rst), .in_data(ALU_result), .out_data(MEM_ALU_result)
-    );
-    Flopr U_EXMEM_RD2  ( .clk(clk), .rst(rst), .in_data(EX_RD2),     .out_data(MEM_RD2) );
-    Flopr U_EXMEM_PCA4 ( .clk(clk), .rst(rst), .in_data(PCA4),    .out_data(MEM_PCA4) );
-    Flopr U_EXMEM_INST ( .clk(clk), .rst(rst), .in_data(EX_Inst),    .out_data(MEM_Inst) );//一直在传inst确实有点浪费了，没必要
-    Flopr U_EXMEM_CTRL ( .clk(clk), .rst(rst), .in_data(ex_ctrl),    .out_data(mem_ctrl) );
+    reg [10:0] ex_mem_ctrl_reg;
+    always @(posedge clk or posedge rst) begin
+        if (rst) ex_mem_ctrl_reg <= 11'b0;
+        else ex_mem_ctrl_reg <= {
+            EX_rd,                              // [10:6] 5位
+            EX_DMCtrl, EX_RFWrite,              // [5:4]  2位
+            EX_WDSel, EX_RegSel                 // [3:0]  4位
+        };
+    end
+    
+    Flopr U_ALUOut (.clk(clk), .rst(rst), .in_data(ALU_result), .out_data(MEM_ALU_result),.CLR(1'b0), .Stall(1'b0));
+    Flopr U_EXMEM_RD2  ( .clk(clk), .rst(rst), .in_data(Forwarded_B),     .out_data(MEM_RD2),.CLR(1'b0), .Stall(1'b0) );
+    Flopr U_EXMEM_PCA4 ( .clk(clk), .rst(rst), .in_data(PCA4),    .out_data(MEM_PCA4),.CLR(1'b0), .Stall(1'b0) );
     
 //————————MEM————————//
-    wire MEM_DMCtrl = mem_ctrl[5]; // 提取 MEM 写使能
+    wire [4:0] MEM_rd      = ex_mem_ctrl_reg[10:6];
+    wire       MEM_DMCtrl  = ex_mem_ctrl_reg[5];
+    wire       MEM_RFWrite = ex_mem_ctrl_reg[4];
+    wire [1:0] MEM_WDSel   = ex_mem_ctrl_reg[3:2];
+    wire [1:0] MEM_RegSel  = ex_mem_ctrl_reg[1:0];
     wire [31:0] RD;
+    
     // 实例化 DM
     DM U_DM (
-        .Addr(MEM_ALU_result[11:2]), .WD(MEM_RD2), .DMCtrl(DMCtrl), .clk(clk), .RD(RD)
+        .Addr(MEM_ALU_result[11:2]), .WD(MEM_RD2), .DMCtrl(MEM_DMCtrl), .clk(clk), .RD(RD)
     );
 //————————MEM/WB————————//
-    wire [31:0] WB_ALU_result, WB_PCA4, WB_Inst, wb_ctrl;
+    wire [31:0] WB_ALU_result, WB_PCA4;
+    
+    reg [9:0] mem_wb_ctrl_reg;
+    always @(posedge clk or posedge rst) begin
+        if (rst) mem_wb_ctrl_reg <= 10'b0;
+        else mem_wb_ctrl_reg <= {
+            MEM_rd,                             // [9:5] 5位
+            MEM_RFWrite, MEM_WDSel, MEM_RegSel  // [4:0] 5位
+        };
+        end
 
-    Flopr U_MEMWB_ALU  ( .clk(clk), .rst(rst), .in_data(MEM_ALU_result), .out_data(WB_ALU_result) );
-    Flopr U_MEMWB_PCA4 ( .clk(clk), .rst(rst), .in_data(MEM_PCA4),       .out_data(WB_PCA4) );
-    Flopr U_MEMWB_INST ( .clk(clk), .rst(rst), .in_data(MEM_Inst),       .out_data(WB_Inst) );
-    Flopr U_MEMWB_CTRL ( .clk(clk), .rst(rst), .in_data(mem_ctrl),       .out_data(wb_ctrl) );
+    Flopr U_MEMWB_ALU  ( .clk(clk), .rst(rst), .in_data(MEM_ALU_result), .out_data(WB_ALU_result),.CLR(1'b0), .Stall(1'b0) );
+    Flopr U_MEMWB_PCA4 ( .clk(clk), .rst(rst), .in_data(MEM_PCA4),       .out_data(WB_PCA4),.CLR(1'b0), .Stall(1'b0) );
     
 //————————WB————————//
-    wire [4:0] WB_rd = WB_Inst[11:7]; // 最终的写回目的寄存器
-    
-    // 提取 WB 阶段控制信号
-    assign WB_RFWrite    = wb_ctrl[4];
-    wire [1:0] WB_WDSel  = wb_ctrl[3:2];
-    wire [1:0] WB_RegSel = wb_ctrl[1:0];
+    wire [4:0] WB_rd      = mem_wb_ctrl_reg[9:5];
+    assign     WB_RFWrite = mem_wb_ctrl_reg[4];
+    wire [1:0] WB_WDSel   = mem_wb_ctrl_reg[3:2];
+    wire [1:0] WB_RegSel  = mem_wb_ctrl_reg[1:0];
     
     // 实例化 MUX_3to1_LMD--决定写回什么数据
     MUX_3to1_LMD U_MUX_3to1_LMD (
-        .X(WB_ALU_result), .Y(RD), .Z(WB_PCA4), .Z_(WB_PCA4[1:0]),
+        .X(WB_ALU_result), .Y(RD), .Z(WB_PCA4[31:2]), .Z_(WB_PCA4[1:0]),
         .control(WB_WDSel), .out(WB_WD)
     );
     
@@ -178,5 +224,32 @@ module riscv(clk, rst);
         .X(WB_rd), .Y(5'd0), .Z(5'd31),
         .control(WB_RegSel), .out(WB_WR)
     );
+    
+//————————harzard unit————————//
+    wire EX_is_Load = (id_ex_ctrl_reg[3:2] == 2'b01); //看看WDSel最终写回的是哪里的数据
+    wire Load_Use_Stall = EX_is_Load && (EX_rd != 5'd0) && ((EX_rd == ID_rs1) || (EX_rd == ID_rs2));
+    
+    assign StallF = EX_is_Load;                       // 暂停 PC
+    assign StallD = EX_is_Load;                       // 暂停 IF/ID
+    assign FlushE = EX_is_Load || EX_Control_Taken;   // 清空 ID/EX (LoadUse时变气泡，跳错时也变气泡)
+    assign FlushD = EX_Control_Taken;              // 清空 IF/ID (只有跳错时需要清空)
+
+//——————forward——————//
+    wire [1:0] ForwardA;
+    wire [1:0] ForwardB;
+        
+    assign ForwardA = (rst == 1'b0) ? 2'b00 : 
+                       ((MEM_RFWrite == 1'b1) & (MEM_rd != 5'h00) & (MEM_rd == EX_rs1)) ? 2'b10 :
+                       ((WB_RFWrite == 1'b1) & (WB_rd != 5'h00) & (WB_rd == EX_rs1)) ? 2'b01 : 2'b00;
+                       
+    assign ForwardB = (rst == 1'b0) ? 2'b00 : 
+                       ((MEM_RFWrite == 1'b1) & (MEM_rd != 5'h00) & (MEM_rd == EX_rs2)) ? 2'b10 :
+                       ((WB_RFWrite == 1'b1) & (WB_rd != 5'h00) & (WB_rd == EX_rs2)) ? 2'b01 : 2'b00;
+    
+    assign Forwarded_A = (ForwardA == 2'b10) ? MEM_ALU_result :
+                         (ForwardA == 2'b01) ? WB_WD : EX_RD1;
+
+    assign Forwarded_B = (ForwardB == 2'b10) ? MEM_ALU_result :
+                         (ForwardB == 2'b01) ? WB_WD : EX_RD2;
     
 endmodule
