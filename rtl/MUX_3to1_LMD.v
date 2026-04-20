86329e3d1da6a43cfc77d77188aeb442baf2a41d
double-kick-firecracker <861129353@qq.com>
2026-04-20 16:13:21 +0800

    Update MUX_3to1_LMD.v

--- rtl/MUX_3to1_LMD.v
+++ rtl/MUX_3to1_LMD.v
@@ -1,11 +1,10 @@
 `timescale 1ns / 1ps
 
 `include "ctrl_signal_def.v"
-module MUX_3to1_LMD(X, Y, Z,Z_, control, out);
+module MUX_3to1_LMD(X, Y, Z, control, out);
     input  [31:0] X;        //临时寄存器ALUOut中的内容
     input  [31:0] Y;        //临时寄存器LMD中的内容
-    input  [31:2] Z;        //PC+4
-    input  [1:0]  Z_;
+    input  [31:0] Z;        //PC+4
     input  [1:0]  control;  //选择控制信号
     output reg [31:0] out;   //输出选择结果
 
@@ -13,7 +12,7 @@ module MUX_3to1_LMD(X, Y, Z,Z_, control, out);
         case(control)
             `WDSel_FromALU  : out = X;  //选择X
             `WDSel_FromMEM  : out = Y;  //选择Y
-            `WDSel_FromPC   : out = {Z,Z_};  //选择Z
+            `WDSel_FromPC   : out = Z;  //选择Z
             `WDSel_Else     : out = 0;
         endcase
     end
